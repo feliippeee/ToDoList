b0340ca086da29bf7e1b09d4e68b1b57147d43aa
feliippeee <felipe.tecnosuporte@gmail.com>
2022-06-22 00:16:24 -0300

    svg

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,10 @@
 import styles from './Header.module.css';
-import Logo from '../assets/logo.svg';
+import { Logo } from './Logo';
 export function Header() {
     return (
         <div className={styles.header}>
             <div className={styles.logo}>
-                <img src={Logo} alt='logotipo do ignite feed' />
+                <Logo />
             </div>
         </div>
     )
